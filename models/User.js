246aeb4d8460bdd6439db11a8b40ec14d9e2d47a
epicasino2 <rayjonathanbadua@gmail.com>
2023-07-07 21:09:29 -0700

    added User.js in models

--- models/User.js
+++ models/User.js
@@ -1,34 +1,44 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 const sequelize = require("../config");
 
-class User extends Model {
-
-}
-User.init({
-  id: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    primaryKey: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [6],
+class User extends Model {}
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
     },
   },
-},
-{
-  hooks: {
-    beforeCreate: async (data) => {
-      data.password = await bcrypt.hash(data.password, 10);
-      return data;
-    }
+  {
+    hooks: {
+      beforeCreate: async (data) => {
+        data.password = await bcrypt.hash(data.password, 10);
+        return data;
+      },
+      beforeUpdate: async (data) => {
+        data.password = await bcrypt.hash(data.password, 10);
+        return data;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "user",
   }
-})
+);
+
+module.exports = User;
